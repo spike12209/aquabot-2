65141304cc65bd62cfebfdd90d7cdfc399d34a30
amiralles <amiralles.net@gmail.com>
2016-12-23 12:17:53 -0300

    Add nodes and lanes

--- src/tests/Aquatests.cs
+++ src/tests/Aquatests.cs
@@ -4,8 +4,50 @@ using System;
 using _ = System.Action<Contest.Core.Runner>;
 
 class Aquatests {
-	_ add_nodes_to_the_move_lane =  assert => {
+	/// Base class for Moves, Changes and Side Effects.
+	class Node {
+		public object Value;
+		public string Label, Notes;
+	}
+
+	/// This nodes are used to represent a move in the "moves lane".
+	class MoveNode : Node {
+
+		/// Points to the next move.
+		public MoveNode Next;
+
+		// +----------------------------------------------------+
+		// | IMPORTANT: Change and Side are mutualy exclusive.  |
+		// | If a change has side effects, those side effects   |
+		// | start at the change node, *NOT* at the move.       |
+		// | Sometimes moves cause side effects, in those cases |
+		// | a move has a side effect but doesn't point to a    |
+		// | change.                                            |
+		// +----------------------------------------------------+
+		/// Points to a change produced by the move.
+		public ChandeNode Change;
+		/// Points to a side effect produced by the move.
+		public SideNode Side;
+		// ------------------------------------------------------
+	}
+
+	/// This nodes are used to represent changes.
+	class ChangeNode : Node {
+		public ChandeNode Next;
+		public SideNode   Side;
+	}
+
+	/// This nodes are used to represent side effects.
+	class SideNode : Node {
+		public SideNode Next;
+	}
+
+	/// DAG representing moves on the form.
+	class MovesLane {
 	
-		
+	}
+
+	_ add_nodes_to_the_move_lane =  assert => {
+
 	};
 }
