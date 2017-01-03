b0854eec1a003d058d6ab7818d8f57a511332ac8
amiralles <amiralles.net@gmail.com>
2017-01-03 16:46:20 -0300

    Add build scripts for windows.

---
+++ build.bat
@@ -0,0 +1,22 @@
+cls
+mkdir ./bin
+
+copy ./lib/parca.dll ./bin/parca.dll
+
+csc ./src/core/*.cs /target:library /out:./bin/aquaforms.dll ^
+	/r:System.Drawing.dll ^
+	/r:./lib/parca.dll ^
+	/r:./lib/Contest.Core.dll ^
+	/r:System.Windows.Forms.dll
+
+csc ./src/app/*.cs /target:exe /out:./bin/aqua.exe ^
+	/r:System.Windows.Forms.dll ^
+	/r:./lib/parca.dll ^
+	/r:./bin/aquaforms.dll
+
+csc ./src/tests/*.cs /target:library /out:./bin/aquatests.dll ^
+	/r:./lib/Contest.Core.dll ^
+	/r:./lib/parca.dll ^
+	/r:./bin/aquaforms.dll ^
+	/r:System.Windows.Forms.dll
+
